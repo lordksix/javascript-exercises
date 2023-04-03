548628830ce6fed6ce73faa21b5c9b4027e8c462
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-03 05:07:54 -0500

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,9 @@
 const palindromes = function (string) {
-    let str = "asdfsdf";
-    str.toLowerCase
     let newStr = string.toLowerCase().replace(/[^a-z]+/g,'');
     let length = newStr.length;
     let itirator = Math.floor(length/2);
     for(let i = 0; i<=itirator;i++){      
-        if(!(newStr [i] === newStr[length-1-i])) return false;
+        if(newStr [i] !== newStr[length-1-i]) return false;
     }
     return true;
 };
